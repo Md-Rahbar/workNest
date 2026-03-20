74c326220efc9110ec5e8badd33bfb515adf5d47
Md Rahbar <rahbaralam747@gmail.com>
2026-03-20 17:30:00 +0530

    registeration of users activated

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,5 +1,44 @@
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer"
 
+//sending email function 
+const sendEmail=async (options) =>{
+    const mailGenerator = new Mailgen({
+        theme:'default',
+        product:{
+           name: "Task Manager",
+           link: "https://taskmanagelink.com" 
+        }
+    })
+    
+    const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
+
+    const emailHtml=mailGenerator.generate(options.mailgenContent);
+
+    const transporter = nodemailer.createTransport({
+        host:process.env.MAILTRAP_SMTP_HOST,
+        port: process.env.MAILTRAP_SMTP_PORT,
+        auth:{
+            user:process.env.MAILTRAP_SMTP_USER,
+            pass: process.env.MAILTRAP_SMTP_PASS
+        }
+    })
+
+    const mail={
+        from : "mail.taskmanager@example.com",
+        to: options.email,
+        subject: options.subject,
+        text: emailTextual,
+        html: emailHtml
+    }
+
+    try {
+        await transporter.sendMail(mail)
+    } catch (error) {
+        console.error("Email service filed silently. Make sure you have provided mail trap credentials in the .env file");
+        console.log("Error: ", error);
+    }
+}
 
 
 const emailVerificationMailgenContent = (username,verificationUrl)=>{
@@ -41,4 +80,4 @@ const forgotPasswordMailgenContent = (username,passwordResrtUrl)=>{
 };
 
 
-export {emailVerificationMailgenContent,forgotPasswordMailgenContent};
+export {emailVerificationMailgenContent,forgotPasswordMailgenContent,sendEmail};
